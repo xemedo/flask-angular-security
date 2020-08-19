857936047077a75eff40fd39abfe12c36c481ec2
NMO13 <martin.ennemoser@gmx.at>
2020-08-19 13:43:14 +0200

    Added schema validation.

--- src/models/article.py
+++ src/models/article.py
@@ -2,6 +2,6 @@ from .. import db
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(255))
+    content = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
                           nullable=False)
